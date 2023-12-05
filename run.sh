f513685c7e812cb115df2acd8966acf5663e7995
Andy Martin <andy@infinium.org.uk>
2023-12-05 12:40:41 +0000

    Switch to new aocbench YAML

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE=esvandiary/aoc2023:4
-
-docker image inspect "${IMAGE}" >/dev/null 2>&1 || docker build .docker -t "${IMAGE}"
-
-exec docker run --rm -i -v "${PWD}:/code" "${IMAGE}" "${@}"
