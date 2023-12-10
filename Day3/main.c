566d81a109c9bb3b345f846c05f696a0ee2a5b25
Andy Martin <andy@infinium.org.uk>
2023-12-10 03:36:17 +0000

    Day3: adjust to a fewer-pass approach

--- Day3/main.c
+++ Day3/main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+// #define ENABLE_DEBUGLOG
 #include "../common/mmap.h"
 #include "../common/print.h"
 #include "../common/vuctor.h"
@@ -8,7 +9,13 @@
 
 #define isdigit(c) ((c) >= '0' && (c) <= '9')
 
-#define dataindex(y, x) ((y * (lineLength + 1)) + x)
+#define dataindex(y, x) (((y) * (lineLength + 1)) + (x))
+
+typedef struct number
+{
+    int number;
+    int length;
+} number;
 
 int main(int argc, char** argv)
 {
@@ -26,31 +33,14 @@ int main(int argc, char** argv)
         }
     }
     const int lineCount = ((textLength + lineLength / 2) / (lineLength + 1));
-    
-    bool* numbers = (bool*)calloc(textLength, sizeof(bool));
-    bool* symbols = (bool*)calloc(textLength, sizeof(bool));
 
-    vuctor gears = VUCTOR_INIT;
-    VUCTOR_RESERVE(gears, int, lineCount * 5);
+    number* numbers = (number*)calloc((uint32_t)textLength, sizeof(number));
 
-    for (int y = 0; y < lineCount; ++y)
-    {
-        for (int x = 0; x < lineLength; ++x)
-        {
-            const int idx = (y * lineLength) + x;
-            char c = file.data[dataindex(y, x)];
-            if (isdigit(c))
-            {
-                numbers[idx] = true;
-            }
-            else if (c != '.')
-            {
-                symbols[idx] = true;
-                if (c == '*')
-                    VUCTOR_ADD(gears, int, idx);
-            }
-        }
-    }
+    int gears[8192];
+    size_t gearCount = 0;
+
+    chartype* data = file.data;
+    chartype* end = file.data + textLength;
 
     //
     // Part 1
@@ -58,50 +48,65 @@ int main(int argc, char** argv)
 
     int sum1 = 0;
 
-    for (int y = 0; y < lineCount; ++y)
+    while (data < end)
     {
-        for (int x = 0; x < lineLength; ++x)
+        const char c = *data;
+        if (!isdigit(c))
         {
-            int startX = x;
-            int startIdx = (y * lineLength + x);
-            int idx = startIdx;
-            int num = 0;
-            while (x < lineLength && numbers[idx])
-            {
-                num *= 10;
-                num += file.data[dataindex(y, x)] & 0xF;
-                ++x;
-                ++idx;
-            }
-            if (idx != startIdx)
-            {
-                if ((startX != 0 && symbols[startIdx - 1]) || (x != lineLength && symbols[idx]))
-                    goto hit;
-                if (y != 0)
-                {
-                    int cidx = ((y - 1) * lineLength) + MAX(0, startX - 1);
-                    int cidxEnd = ((y - 1) * lineLength) + MIN(lineLength - 1, x);
-                    while (cidx <= cidxEnd)
-                    {
-                        if (symbols[cidx++])
-                            goto hit;
-                    }
-                }
-                if (y + 1 != lineCount)
-                {
-                    int cidx = ((y + 1) * lineLength) + MAX(0, startX - 1);
-                    int cidxEnd = ((y + 1) * lineLength) + MIN(lineLength - 1, x);
-                    while (cidx <= cidxEnd)
-                    {
-                        if (symbols[cidx++])
-                            goto hit;
-                    }
-                }
-            }
+            if (c == '*')
+                gears[gearCount++] = data - file.data;
+            ++data;
             continue;
-        hit:
-            sum1 += num;
         }
+
+        chartype* numend = data + 1;
+        int num = *data & 0xF;
+        while (numend < end && isdigit(*numend))
+        {
+            num *= 10;
+            num += *numend & 0xF;
+            ++numend;
+        }
+
+        const size_t idx = data - file.data;
+        const size_t len = numend - data;
+        numbers[idx] = (number) { .number = num, .length = len };
+
+        if ((idx != 0 && data[-1] != '.' && data[-1] != '\n') || (numend[0] != '.' && numend[0] != '\n'))
+        {
+            sum1 += num;
+            goto next;
+        }
+        if (idx >= lineLength + 1)
+        {
+            chartype* ccur = file.data + MAX(0, idx - lineLength - 2);
+            chartype* cend = file.data + idx + len - lineLength;
+            while (ccur < cend)
+            {
+                if (*ccur != '.' && *ccur != '\n')
+                {
+                    sum1 += num;
+                    goto next;
+                }
+                ++ccur;
+            }
+        }
+        if (numend + lineLength + 1 < end)
+        {
+            chartype* ccur = file.data + idx + lineLength;
+            chartype* cend = file.data + idx + len + lineLength + 2;
+            while (ccur < cend)
+            {
+                if (*ccur != '.' && *ccur != '\n')
+                {
+                    sum1 += num;
+                    goto next;
+                }
+                ++ccur;
+            }
+        }
+    next:
+        data = numend;
     }
 
     print_uint64(sum1);
@@ -112,37 +117,30 @@ int main(int argc, char** argv)
 
     int sum2 = 0;
 
-    for (int gi = 0; gi < gears.size; ++gi)
+    for (int gi = 0; gi < gearCount; ++gi)
     {
-        int gidx = VUCTOR_GET(gears, int, gi);
-        int y = gidx / lineLength;
-        int x = gidx % lineLength;
+        int gidx = gears[gi];
+        int y = gidx / (lineLength + 1);
+        int x = gidx % (lineLength + 1);
 
         int numCount = 0;
         int mul = 1;
 
         for (int cy = MAX(0, y - 1); cy <= MIN(lineCount - 1, y + 1); ++cy)
         {
-            for (int cx = MAX(0, x - 1); cx <= MIN(lineLength - 1, x + 1); ++cx)
+            for (int cx = MAX(0, x - 1); cx <= x + 1; ++cx)
             {
-                if (numbers[(cy * lineLength) + cx])
+                size_t idx = dataindex(cy, cx);
+                if (isdigit(file.data[idx]))
                 {
-                    // find start of number
-                    int startX = cx;
-                    while (startX >= 1 && numbers[(cy * lineLength) + startX - 1])
-                        --startX;
-                    int endX = startX;
-                    int num = 0;
-                    while (endX < lineLength && numbers[(cy * lineLength) + endX])
+                    while (!numbers[idx].length)
                     {
-                        num *= 10;
-                        num += file.data[dataindex(cy, endX)] & 0xF;
-                        ++endX;
+                        --idx;
+                        --cx;
                     }
-
-                    mul *= num;
+                    mul *= numbers[idx].number;
                     ++numCount;
-                    cx = endX;
+                    cx += numbers[idx].length;
                 }
             }
         }
