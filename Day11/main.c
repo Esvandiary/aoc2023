c26d844f7a277348e074cfc7bee35324cd3181b7
Andy Martin <andy@infinium.org.uk>
2023-12-11 15:14:40 +0000

    Day11: stop being dumb, just subtract (dist[0-n] - dist[0-m])

--- Day11/main.c
+++ Day11/main.c
@@ -21,10 +21,10 @@ static uint8_t widths1[512] = { [0 ... 511] = 2 };
 static uint8_t heights1[512] = { [0 ... 511] = 2 };
 static uint32_t widths2[512] = { [0 ... 511] = 1000000 };
 static uint32_t heights2[512] = { [0 ... 511] = 1000000 };
-static int16_t ydistances1[512*512] = { 0 };
-static int32_t ydistances2[512*512] = { 0 };
-static int16_t xdistances1[512*512] = { 0 };
-static int32_t xdistances2[512*512] = { 0 };
+static int16_t ydistances1[512] = { 0 };
+static int32_t ydistances2[512] = { 0 };
+static int16_t xdistances1[512] = { 0 };
+static int32_t xdistances2[512] = { 0 };
 
 int main(int argc, char** argv)
 {
@@ -58,31 +58,21 @@ int main(int argc, char** argv)
     }
 
     const int ymax = (fileSize / lineLength) + 1;
-    for (int y1 = 0; y1 < ymax - 1; ++y1)
+    int yd1 = 0, yd2 = 0;
+    for (int y = 0; y < ymax; ++y)
     {
-        if (heights1[y1] == 2)
-            continue;
-        int yd1 = 0, yd2 = 0;
-        for (int y2 = y1 + 1; y2 < ymax; ++y2)
-        {
-            yd1 += heights1[y2];
-            yd2 += heights2[y2];
-            ydistances1[(y1 << 9) | y2] = ydistances1[(y2 << 9) | y1] = yd1;
-            ydistances2[(y1 << 9) | y2] = ydistances2[(y2 << 9) | y1] = yd2;
-        }
+        yd1 += heights1[y];
+        yd2 += heights2[y];
+        ydistances1[y] = yd1;
+        ydistances2[y] = yd2;
     }
-    for (int x1 = 0; x1 < lineLength - 2; ++x1)
+    int xd1 = 0, xd2 = 0;
+    for (int x = 0; x < lineLength - 1; ++x)
     {
-        if (widths1[x1] == 2)
-            continue;
-        int xd1 = 0, xd2 = 0;
-        for (int x2 = x1 + 1; x2 < lineLength - 1; ++x2)
-        {
-            xd1 += widths1[x2];
-            xd2 += widths2[x2];
-            xdistances1[(x1 << 9) | x2] = xdistances1[(x2 << 9) | x1] = xd1;
-            xdistances2[(x1 << 9) | x2] = xdistances2[(x2 << 9) | x1] = xd2;
-        }
+        xd1 += widths1[x];
+        xd2 += widths2[x];
+        xdistances1[x] = xd1;
+        xdistances2[x] = xd2;
     }
 
     DSTOPWATCH_END(init);
@@ -101,14 +91,14 @@ int main(int argc, char** argv)
             const int y1 = dataY(galaxies[i]), y2 = dataY(galaxies[j]);
             if (y1 != y2)
             {
-                sum1 += ydistances1[(y1 << 9) | y2];
-                sum2 += ydistances2[(y1 << 9) | y2];
+                sum1 += abs(ydistances1[y2] - ydistances1[y1]);
+                sum2 += abs(ydistances2[y2] - ydistances2[y1]);
             }
             const int x1 = dataX(galaxies[i]), x2 = dataX(galaxies[j]);
             if (x1 != x2)
             {
-                sum1 += xdistances1[(x1 << 9) | x2];
-                sum2 += xdistances2[(x1 << 9) | x2];
+                sum1 += abs(xdistances1[x2] - xdistances1[x1]);
+                sum2 += abs(xdistances2[x2] - xdistances2[x1]);
             }
         }
     }
